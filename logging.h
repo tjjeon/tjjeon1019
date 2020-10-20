ff5f7d43a088c814c28c029709ae3403240260f7
tjjeon <71472013+tjjeon@users.noreply.github.com>
2020-10-20 14:59:40 +0900

    Add files via upload

--- logging.h
+++ logging.h
@@ -1,58 +1,137 @@
+
+
+// 1. 모듈 또는 소스 파일의 어떤 함수, 어떤 위치에서 발생한 정보인지 알수 있어야 한다.
+// 2. 로깅 정보가 다양한 목적지로 저장 / 전송 될 수 있어야 한다./
+// 3. 로깅의 출력 레벨을 조정할 수 있어야 한다.
+// 4. 로깅의 정확한 날짜와 시간을 기록할 수 있어야 한다.
+// 5. 로깅을 날짜별 시간별로 다른 파일로 기록할 것.
+// 6. 로그 파일이 지정된 크기를 넘어설 경우, 자동으로 다른 파일에 기록할 수 있어야 한다.
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <ctime>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <iomanip>
 #include <stdio.h>
-using namespace std;
-
-#define LOG_DEBUG	logdisp(DEBUG		,__FILE__,__FUNCTION__,__LINE__)
-#define LOG_TRACE	logdisp(TRACE		,__FILE__,__FUNCTION__,__LINE__)
-#define LOG_INFO	logdisp(INFO		,__FILE__,__FUNCTION__,__LINE__)
-#define LOG_WARNING logdisp(WARNING		,__FILE__,__FUNCTION__,__LINE__)
-#define LOG_ERROR	logdisp(ERROR		,__FILE__,__FUNCTION__,__LINE__)
-#define LOG_FATAL	logdisp(FATAL		,__FILE__,__FUNCTION__,__LINE__)
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <time.h>
 
 
-void logdisp(const int& s, const char* fileName_, const char* funcName_, const int& line_) {
-    Log* m = new Log(fileName_, funcName_, line_);
-    m->Logprint();
+
+enum severity {
+	ERROR,
+	WARN,
+	INFO,
+	DEBUG,
+	TRACE
+};
+#define LOG(level)  logprint(level,__FILE__,__FUNCTION__,__LINE__);  
+
+
+int GetSize(std::string s) {
+
+	int size = 0;
+	FILE* fp = fopen(s.c_str(), "r");
+	if (fp == nullptr) {
+		return 0;
+	}
+
+	fseek(fp, 0, SEEK_END);    
+	size = ftell(fp);          
+
+	fclose(fp);
+	printf("사이즈는 : % d", size);
+	return size;
 }
 
 
-enum LogLevel {
-    DEBUG = 0,
-    TRACE = 1,
-    INFO = 2,
-    WARN = 3,
-    ERROR = 4,
-    FATAL = 5
-};
-
-class LoggingEvent : public ostringstream {
-private:
-    string _module;
-    LogLevel _level;
-    string _file;
-    string _func;
-    int _line;
-
-    // Prohibit invocation of default constructor
-    LoggingEvent();
+class Log {
+	std::string log_info;
+	std::string filePath;
+	std::string information;
+	std::string file_name;
+	std::string func_name;
+	std::string text_name;
+	std::string time_info;
+	int line_num;
+	int year;
+	int month;
+	int day;
+	int hour;
+	int minute;
+	int second;
 
 public:
-    LoggingEvent(
-        const char* mod, LogLevel lvl, const char* file,
-        const char* func, int line,
-        const char* msg = ""
-    ) :
-        ostringstream(), _module(mod), _level(lvl), _file(file), _func(func), _line(line)
-    {
-        *this << msg;
-    }
+	Log(std::string info, std::string fn, std::string fcn, int lnum) {
+		time_t t = time(NULL);
+		struct tm tm = *localtime(&t);
+		information = info;
+		year = tm.tm_year + 1900;
+		month = tm.tm_mon + 1;
+		day = tm.tm_mday;
+		hour = tm.tm_hour;
+		minute = tm.tm_min;
+		second = tm.tm_sec;
+		file_name = fn;
+		func_name = fcn;
+		line_num = lnum;
 
- 
+		time_info = std::to_string(year) + "/" + std::to_string(month) + "/"
+			+ std::to_string(day) + "/" + "]" + ".txt";
+
+		text_name = information + "_" + "[" + std::to_string(year) + " " + std::to_string(month) +
+			" " + std::to_string(day) + "  " + "]" + ".txt";
+
+		log_info = "[" + information + "] " + file_name + " " + func_name + " " + std::to_string(line_num) + " " +
+			"[ date: " + std::to_string(year) + "/" + std::to_string(month) + "/" + std::to_string(day) + "/  time : " + std::to_string(hour) +
+			"h" + std::to_string(minute) + "m" + std::to_string(second) + "s" + "]";
+	}
+
+	void Command() {
+
+		Write(text_name);
+
+	}
+
+	void Write(std::string filePath) {
+		FILE* fp = fopen(filePath.c_str(), "a");
+		if (fp == nullptr) {
+			fprintf(stderr, "File Open Error\n");
+		};
+		std::cout << log_info << std::endl;
+		fprintf(fp, log_info.c_str());
+		fflush(fp);
+
+		//int size = GetSize(filePath);
+
+		//std::cout << "file size" << size << std::endl;
+		//std::ofstream writeFile(filePath.data(), std::ios::app);
+		//if (writeFile.is_open()) {
+		//	writeFile << log_info << "\n";
+		//}
+		//writeFile.close();
+
+
+		//std::cout << log_info << std::endl;
+	}
 
 };
 
+void logprint(int which_log, const char* filename, const char* funcname, const int& linenum) {
+	std::string s;
+
+	switch (which_log)
+	{
+	case(0): s = "ERROR"; break;
+	case(1): s = "WARN"; break;
+	case(2): s = "INFO"; break;
+	case(3): s = "DEBUG"; break;
+	case(4): s = "TRACE"; break;
+
+	default:
+		break;
+	}
+	
+
+	Log* m = new Log(s, filename, funcname, linenum);
+	m->Command();
+}
---
+++ main.cpp
@@ -0,0 +1,136 @@
+// 1. 모듈 또는 소스 파일의 어떤 함수, 어떤 위치에서 발생한 정보인지 알수 있어야 한다.
+// 2. 로깅 정보가 다양한 목적지로 저장 / 전송 될 수 있어야 한다./
+// 3. 로깅의 출력 레벨을 조정할 수 있어야 한다.
+// 4. 로깅의 정확한 날짜와 시간을 기록할 수 있어야 한다.
+// 5. 로깅을 날짜별 시간별로 다른 파일로 기록할 것.
+// 6. 로그 파일이 지정된 크기를 넘어설 경우, 자동으로 다른 파일에 기록할 수 있어야 한다.
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <time.h>
+#include <vector>
+
+#include "logging.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+int keyinput() {
+
+    while (1) {
+        getchar();
+        LOG(INFO);
+        getchar();
+        LOG(DEBUG);
+        getchar();
+        LOG(ERROR);
+    }
+}
+
+
+class BaseMenu {
+    std::string title;
+public:
+    BaseMenu(const std::string& s) : title(s) {}
+    virtual ~BaseMenu() {}
+
+    std::string getTitle() const { 
+        if (title == "키보드 입력 로깅")
+        {
+            //cout << "키보드" << endl;
+
+            //getchar();
+            //LOG(INFO);
+            keyinput();
+        }
+        return title; }
+
+    virtual void command() = 0;
+};
+
+class MenuItem : public BaseMenu {
+public:
+    MenuItem(const std::string& s) : BaseMenu(s) {}
+
+    void command() override {
+        cout << getTitle() << "이 선택됨" << endl;
+        char keyinput =getchar();
+        //getchar();
+        //fflush(keyinput);
+    }
+};
+
+class PopupMenu : public BaseMenu {
+    std::vector<BaseMenu*> v;          // 재귀적 포함
+public:
+    PopupMenu(const std::string& s) : BaseMenu(s) {}
+
+    void addMenu(BaseMenu* p) { v.push_back(p); }
+
+    void command() override {
+        while (1) {
+            system("cls");  // system("cls");
+            int sz = v.size();
+            for (int i = 0; i < sz; ++i) {
+                cout << i + 1 << ". " << v[i]->getTitle() << endl;
+            }
+            cout << sz + 1 << ". 상위 메뉴로" << endl;
+
+            cout << "메뉴를 선택하세요 >> ";
+            int cmd;
+            cin >> cmd;
+
+            if (cmd == sz + 1)
+            {
+               // cout << "cmd : " << cmd << endl;
+                break;
+            }
+
+            if (cmd < 1 || cmd > sz + 1) 
+            { 
+                //cout << "cmd : " << cmd << endl;
+                continue;
+            }
+
+          
+            v[cmd - 1]->command();  
+           
+        }
+        
+    }
+};
+
+int main() {
+    PopupMenu* menubar = new PopupMenu("MENUBAR");
+    PopupMenu* p1 = new PopupMenu("로깅");
+    PopupMenu* p2 = new PopupMenu("로깅 출력 레벨 조정");
+    PopupMenu* p3 = new PopupMenu("도움말");
+
+
+    menubar->addMenu(p1);
+    menubar->addMenu(p2);
+    menubar->addMenu(p3);
+
+    p1->addMenu(new MenuItem("키보드 입력 로깅"));
+    p1->addMenu(new MenuItem("랜덤 입력 로깅"));
+    p1->addMenu(new MenuItem("파일 입력 로깅"));
+
+    p2->addMenu(new MenuItem("ERROR 메시지만 출력"));
+    p2->addMenu(new MenuItem("WARN,ERROR 메시지 출력"));
+    p2->addMenu(new MenuItem("INFO, WARN,ERROR 메시지 출력"));
+    p2->addMenu(new MenuItem("DEBUG, INFO, WARN,ERROR 메시지 출력"));
+    p2->addMenu(new MenuItem("TRACE, DEBUG, INFO, WARN,ERROR 메시지 출력"));
+
+    p3->addMenu(new MenuItem("로깅 메시지 띄우는 법"));
+    p3->addMenu(new MenuItem("로그 레벨"));
+
+
+
+    menubar->command();
+    
+}
