e7ca104530617ab7115d74dbd0634bdee6a4d007
tjjeon <71472013+tjjeon@users.noreply.github.com>
2020-10-22 14:10:11 +0900

    Update Timestamp.h

--- Timestamp.h
+++ Timestamp.h
@@ -23,23 +23,3 @@ private:
     int minute;
     int second;
 };
-
-class FileTimestamp {
-public:
-    FileTimestamp(int y, int m, int d, int h)
-        : year(y), month(m), day(d), hour(h) {}
-
-    static FileTimestamp current();
-
-
-    // "2020.10.20 08:20:35"
-    std::string toString() const;
-
-private:
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
-    int second;
-};
